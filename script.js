179d3e2aadd69ae1bcbaf55ea2cf83dd6b7ddc16
mr.nobody <id4acc2014@protonmail.com>
2023-12-27 23:08:11 +0330

    contact and footer

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', function(){
-    header.classList.toggle('sticky', window.scrollY > 70);
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 70);
 });
